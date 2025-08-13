15b9ea5fb53e375d10f1e5c90ff8b00212683fa4
Wilton Batiquin <cbasher24@gmail.com>
2025-08-13 12:06:55 +0800

    Separated CartItem into another component

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -4,6 +4,7 @@ import { currencyFormatter } from "../utils/formatter";
 import Button from "./common/Button";
 import { CartContext } from '../store/cartStore';
 import { UserProgressContext } from "../store/UserProgressContext";
+import CartItem from "./CartItem";
 
 export default function Cart() {
     const cartCtx = useContext(CartContext);
@@ -13,16 +14,25 @@ export default function Cart() {
         userProgressCtx.hideCart();
     }
 
+    console.log(cartCtx.cartItem);
+
 
     return (
         <Modal open={userProgressCtx.progress === 'cart'} className="cart">
             <h2>Your Cart</h2>
             <ul>
                 {cartCtx.cart.map(item => (
-                    <li key={item.id}>{item.name}</li>
+                    <CartItem
+                        key={item.id}
+                        name={item.name}
+                        quantity={item.quantity}
+                        price={item.price}
+                        onIncrease={() => cartCtx.addItem(item)}
+                        onDecrease={() => cartCtx.removeItem(item.id)}
+                    />
                 ))}
             </ul>
-            <p className="cart-total">Total: ${currencyFormatter.format(cartCtx.cartItem.reduce((total, item) => total + item.price, 0).toFixed(2))} </p>
+            <p className="cart-total">Total: ${currencyFormatter.format(cartCtx.cart.reduce((total, item) => total + item.price, 0))} </p>
             <p className="modal-actions">
                 <Button onClick={handleCloseCart}>Checkout</Button>
                 <Button onClick={handleCloseCart}>Remove</Button>
